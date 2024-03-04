980a5e8f64d4e6760203c1df820fe9130971ea6b
Santje777 <s.v.oosterom@gmail.com>
2024-03-04 15:20:39 +0100

    Added basic form handling and added Typewriter js plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("hi");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "c'que tu penses de moi",
+    autoStart: true,
+    delay: 20,
+    cursor: null,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
